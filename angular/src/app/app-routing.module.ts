0c3e1f9c288548f4135a2f56c2981cf4245f9dbc
Neeraj4141 <mewadaneeraj368@gmail.com>
2026-05-12 18:57:04 +0530

    done

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'welcome'
+  },
+  {
+    path:'welcome',
+    component: WelcomeComponent
+  },
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
